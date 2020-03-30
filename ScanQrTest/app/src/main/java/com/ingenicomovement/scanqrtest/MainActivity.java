cbbb1c654fe54c4eb93b15e01a52e74ec6837599
CindoddCindy <cindodcindy@gmail.com>
2020-03-30 19:11:11 +0700

    library eror

--- ScanQrTest/app/src/main/java/com/ingenicomovement/scanqrtest/MainActivity.java
+++ ScanQrTest/app/src/main/java/com/ingenicomovement/scanqrtest/MainActivity.java
@@ -1,14 +1,74 @@
 package com.ingenicomovement.scanqrtest;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ViewUtils;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonScan;
+    private TextView textViewNama, textViewTinggi;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonScan = (Button) findViewById(R.id.buttonScan);
+        textViewNama = (TextView) findViewById(R.id.textViewNama);
+        textViewTinggi = (TextView) findViewById(R.id.textViewTinggi);
+
+        buttonScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              IntentIntegrator  intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.initiateScan();
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null){
+            if (result.getContents() == null){
+                Toast.makeText(this, "Hasil tidak ditemukan", Toast.LENGTH_SHORT).show();
+            }else{
+                // jika qrcode berisi data
+                try{
+                    // converting the data json
+                    JSONObject object = new JSONObject(result.getContents());
+                    // atur nilai ke textviews
+                    textViewNama.setText(object.getString("nama"));
+                    textViewTinggi.setText(object.getString("tinggi"));
+                }catch (JSONException e){
+                    e.printStackTrace();
+                    // jika format encoded tidak sesuai maka hasil
+                    // ditampilkan ke toast
+                    Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }else{
+
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+
+
     }
 }
